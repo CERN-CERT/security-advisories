4380e62e2388b08bacf3a9d56a73b0f126581ae2
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2020-06-09 18:24:13 +0200

    SSO
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- db.py
+++ db.py
@@ -7,31 +7,35 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Post(Base):
-   __tablename__ = 'post'
 
-   id = Column(Integer, primary_key=True)
-   title = Column(String(100))
-   body = Column(String(100000))
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100))
+    body = Column(String(100000))
+
 
 class Link(Base):
-   __tablename__ = 'link'
+    __tablename__ = 'link'
 
-   id = Column(Integer, primary_key=True)
-   link_for = Column(String(100))
-   uid = Column(String(100))
-   post_id = Column(Integer, ForeignKey('post.id'))
-   post = relationship('Post', backref=backref('links'))
+    id = Column(Integer, primary_key=True)
+    link_for = Column(String(100))
+    uid = Column(String(100))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('Post', backref=backref('links'))
+
 
 class Visit(Base):
-   __tablename__ = 'visit'
+    __tablename__ = 'visit'
 
-   id = Column(Integer, primary_key=True)
-   link_id = Column(Integer, ForeignKey('link.id'))
-   link = relationship('Link', backref=backref('visits'))
-   dt = Column(String(100))
-   ip = Column(String(100))
-   ref = Column(String(1000))
+    id = Column(Integer, primary_key=True)
+    link_id = Column(Integer, ForeignKey('link.id'))
+    link = relationship('Link', backref=backref('visits'))
+    dt = Column(String(100))
+    ip = Column(String(100))
+    ref = Column(String(1000))
+
 
 engine = create_engine(os.getenv('DB_URL'))
 logging.info('Creating tables')
--- wsgi.py
+++ wsgi.py
@@ -5,16 +5,59 @@ import logging
 import markdown2
 from datetime import datetime
 from logging.config import dictConfig
-from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, make_response
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response, current_app
 from db import get_session, Post, Link, Visit
 from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+from oauthlib.oauth2.rfc6749.errors import InvalidGrantError, TokenExpiredError
 
 application = Flask(__name__)
 application.wsgi_app = ProxyFix(application.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 
 application.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 application.config['SERVER_NAME'] = os.getenv('SERVER_NAME')
-application.config['PREFERRED_URL_SCHEME'] = 'https'
+
+
+oauth = OAuth2ConsumerBlueprint(
+    'cern_oauth',
+    __name__,
+    client_id=os.getenv('OAUTH_CLIENT_ID'),
+    client_secret=os.getenv('OAUTH_CLIENT_SECRET'),
+    token_url='https://oauth.web.cern.ch/OAuth/Token',
+    authorization_url='https://oauth.web.cern.ch/OAuth/Authorize',
+    login_url='/oauth/cern',
+    authorized_url='/oauth/cern/authorized',
+)
+application.oauth = oauth
+application.register_blueprint(oauth)
+
+
+def require_auth(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+        if current_app.oauth.token:
+            try:
+                user_details = oauth.session.get(
+                    'https://oauthresource.web.cern.ch/api/User')
+                user_details.raise_for_status()
+                # user_data = user_details.json()
+                # username = user_data['username'].strip()
+                egroup_membership = current_app.oauth.session.get(
+                    'https://oauthresource.web.cern.ch/api/Groups')
+                egroup_membership.raise_for_status()
+                groups = egroup_membership.json()['groups']
+                logging.debug('OAuth groups: %s', groups)
+                if any(group in groups for group in current_app.config['app']['auth_egroups']):
+                    return func(*args, **kwargs)
+                else:
+                    return 'Unauthorized', 401
+            except (InvalidGrantError, TokenExpiredError) as e:
+                logging.warn(e)
+                pass
+        return redirect(url_for('cern_oauth.login'))
+    wrapper.func_name = func.func_name
+    return wrapper
+
 
 dictConfig({
     'version': 1,
@@ -32,7 +75,9 @@ dictConfig({
     }
 })
 
-@application.route('/sekkreturl/info/<pid>')
+
+@require_auth
+@application.route('/info/<pid>')
 def info(pid):
     s = get_session()
     try:
@@ -52,7 +97,9 @@ def info(pid):
     finally:
         s.close()
 
-@application.route('/sekkreturl/admin')
+
+@require_auth
+@application.route('/admin')
 def admin():
     s = get_session()
     posts = [(p.id, p.title) for p in s.query(Post).all()]
@@ -60,9 +107,11 @@ def admin():
     logging.info(posts)
     return render_template('admin.html', posts=posts)
 
-@application.route('/sekkreturl/newlink', methods=['POST'])
+
+@require_auth
+@application.route('/newlink', methods=['POST'])
 def newlink():
-    uid = str(uuid.uuid1()).replace('-','')
+    uid = str(uuid.uuid1()).replace('-', '')
     link_for = request.form['linkfor']
     post_id = int(request.form['post_id'])
     s = get_session()
@@ -76,7 +125,9 @@ def newlink():
     finally:
         s.close()
 
-@application.route('/sekkreturl/send', methods=['POST'])
+
+@require_auth
+@application.route('/send', methods=['POST'])
 def send():
     title = request.form['title']
     body = request.form['md']
@@ -89,9 +140,10 @@ def send():
     finally:
         s.close()
 
+
 @application.route('/<uid>')
 def view(uid):
-    logging.info('Viewing uid %s', uid)
+    logging.info('Viewing uid %s ip %s ref %s', uid, request.remote_addr, request.referrer)
     s = get_session()
     try:
         link = s.query(Link).filter_by(uid=uid).first()
@@ -110,6 +162,7 @@ def view(uid):
         return resp
     finally:
         s.close()
+
 
 if __name__ == "__main__":
     application.run()
